db3506532e1088c6b1933295fb95a5a67382e21d
Beatriz Salles <beatrizsallesss@gmail.com>
2023-07-15 19:15:55 -0300

    :recycle: Create app factory

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
 from flask import Flask
 from flask_restful import  Api
 from resources.transaction import Transactions, Transaction
+from database import database
+import cors
 
-app = Flask(__name__)
-api = Api(app)
+# app = Flask(__name__)
+# api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ewallet.db'
-app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ewallet.db'
+# app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.before_request
-def create_database():
-    database.create_all()
+# @app.before_request
+# def create_database():
+#     database.create_all()
 
-api.add_resource(Transactions, '/transactions')
-api.add_resource(Transaction, '/transactions/<string:uid>')
+# api.add_resource(Transactions, '/transactions')
+# api.add_resource(Transaction, '/transactions/<string:uid>')
+
+# if __name__ == '__main__':
+#     from database import database
+    
+#     database.init_app(app)
+#     app.run(debug=True)
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ewallet.db'
+    app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+
+    api.add_resource(Transactions, '/transactions')
+    api.add_resource(Transaction, '/transactions/<string:uid>')
+
+    cors.init(app)
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
-    from database import database
+    import database
     
-    database.init_app(app)
+    database.init(app)
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,3 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 database = SQLAlchemy()
+
+def init(app):
+    database.init_app(app)
+    
